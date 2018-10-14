c5866b8b1430b9239ce5ddc881ea6f76b9032547
thethingyk <thethingyk@gmail.com>
2018-10-15 00:35:03 +0800

    update order

--- src/main/java/com/cafebabe/phosphor/dao/GroupCourseDAO.java
+++ src/main/java/com/cafebabe/phosphor/dao/GroupCourseDAO.java
@@ -2,8 +2,31 @@ package com.cafebabe.phosphor.dao;
 
 import com.cafebabe.phosphor.model.entity.GroupCourse;
 
+import java.util.List;
+
 /**
  * GroupCourseDAO继承基类
  */
 public interface GroupCourseDAO extends MyBatisBaseDao<GroupCourse, Integer> {
+
+    /**
+     * 给套餐插入课程
+     * @param groupCourse 套餐的课程
+     * @return
+     */
+    Integer insertGroupCourse(GroupCourse groupCourse);
+
+    /**
+     * 删除套餐的课程
+     * @param id
+     * @return
+     */
+    Integer removeGroupCourse(Integer id);
+
+    /**
+     * 查询套餐的所有课程
+     * @param groupId 套餐编号
+     * @return
+     */
+    List<GroupCourse> getGroupCourse(Integer groupId);
 }
