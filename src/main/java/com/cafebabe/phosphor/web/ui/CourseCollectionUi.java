3366188f8a651233a79db28cb5114b09e1cae89f
withoutwater <weijincong@outlook.com>
2018-11-05 10:00:42 +0800

    收藏与购买点击事件

--- src/main/java/com/cafebabe/phosphor/web/ui/CourseCollectionUi.java
+++ src/main/java/com/cafebabe/phosphor/web/ui/CourseCollectionUi.java
@@ -2,11 +2,9 @@ package com.cafebabe.phosphor.web.ui;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
